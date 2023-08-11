efe96e1e024110a3e262fa30f0c560204cbc0ecf
Gridin_Andrei <andreigridin918@gmail.com>
2023-08-11 23:02:16 +0500

    Добавьте файлы проекта.

---
+++ ArrUser/ArrUser.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int main()
+{
+    system("chcp 1251 > nul");
+    const int n = 10;
+    int i = 0;
+    int arr[n];
+    while (i < n) {
+        if (i == 0) {
+            cout << "Введите число для первой ячейки массива: ";
+            cin >> arr[i];
+        }
+        else {
+            cout << "Введите число для следующей ячейки массива: ";
+            cin >> arr[i];
+        }
+        i++;
+    }
+    cout << endl;
+    int j = 0;
+    while (j < n) {
+        cout << arr[j] << " ";
+        j++;
+    }
+    cout << "Массив заполнен!" << endl;
+    system("pause > nul");
+    return 0;
+}
